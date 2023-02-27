7196087ccbeb460b10407157f451b0de406ccafe
sergeyinstigatemobile <sergey@instigatemobile.com>
2023-02-27 16:21:28 +0400

    CON-53: Make Journey to partnership section non clickable

---
+++ viva-child/js/custom.js
@@ -0,0 +1,10 @@
+(function ($) {
+  $(document).ready(function () {
+    ("use strict");
+
+    // Make Journey to partnership section non clickable
+    $(".make-non-clickable").click(function (event) {
+      event.preventDefault();
+    });
+  });
+})(jQuery);
