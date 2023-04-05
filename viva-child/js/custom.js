9174209db0c4e52ff3a07b6023b0a421366c7270
sergeyinstigatemobile <sergey@instigatemobile.com>
2023-04-05 18:04:01 +0400

    CON-92: Move fontawesome icons to local

--- viva-child/js/custom.js
+++ viva-child/js/custom.js
@@ -7,5 +7,9 @@
       return false;
     });
 
+    /* Add Journey to Partnership section icons */    
+    $('.fa-puzzle-piece').removeClass('fas fa-puzzle-piece').addClass('fa-light fa-puzzle');
+    $('.fa-handshake').removeClass('far fa-handshake').addClass('fa-light fa-hands-holding-diamond'); 
+
   });
 })(jQuery);
