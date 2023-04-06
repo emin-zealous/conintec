6017ad59d62ab7a6354b987d3da95b71738da4e4
sergeyinstigatemobile <sergey@instigatemobile.com>
2023-04-06 16:06:27 +0400

    CON-86: Add images for workflow section

--- viva-child/js/custom.js
+++ viva-child/js/custom.js
@@ -2,10 +2,18 @@
   $(document).ready(function () {
     ("use strict");
 
-    // Make Polylang menu icon non clickable
-    $(".pll-parent-menu-item > a").click(function () {
-      return false;
-    });
-
+    // Add images for workflow section
+    $(".first-image > div > a > .timeline-item-date").append(
+      '<img width="25%" height="auto" style="max-width: 80px; margin: -10px" src="/wp-content/uploads/2023/03/foto_telescope_284-1-1.jpg">'
+    );
+    $(".second-image > div > a > .timeline-item-date").append(
+      '<img width="25%" height="auto" style="max-width: 80px; margin: -10px" src="/wp-content/uploads/2023/03/foto_puzzle_284-1-1.jpg">'
+    );
+    $(".thirdth-image > div > a > .timeline-item-date").append(
+      '<img width="25%" height="auto" style="max-width: 80px; margin: -10px" src="/wp-content/uploads/2023/03/computer_hand_288x284.png">'
+    );
+    $(".fourth-image > div > a > .timeline-item-date").append(
+      '<img width="25%" height="auto" style="max-width: 80px; margin: -10px" src="/wp-content/uploads/2023/03/Hands-1-1.jpg">'
+    );
   });
 })(jQuery);
