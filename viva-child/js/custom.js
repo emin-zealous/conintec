ec84e4cdb9b63b9e622973291909a68689305cbf
sergeyinstigatemobile <sergey@instigatemobile.com>
2023-04-06 15:49:50 +0400

    CON-118: Arrow positions in the Nearshore Services sections in the mobile view

--- viva-child/js/custom.js
+++ viva-child/js/custom.js
@@ -2,10 +2,25 @@
   $(document).ready(function () {
     ("use strict");
 
-    // Make Polylang menu icon non clickable
-    $(".pll-parent-menu-item > a").click(function () {
-      return false;
+    // Arrows position in Nearshore Services sections
+    function imageDementions() {
+      const img = $(
+        "#nearshore-images > div > div > div > div > div > div > div.owl-item.active > div > div.photobox-img > img"
+      );
+      const arrowLength = $(
+        "#nearshore-images > div > div > div > div > div.owl-wrapper-outer"
+      );
+      // Set the top and width properties of your target div to the image height
+      $(".owl-nav").css({
+        top: img.height() / 2 - 35 + "px",
+        "max-width": arrowLength.width() - 12,
+      });
+    }
+    imageDementions();
+    $(window).resize(function () {
+      setTimeout(() => {
+        imageDementions();
+      }, 1000); // set timer to wait 1s after resize event has finished
     });
-
   });
 })(jQuery);
