11b587fb3d74f1291a0891112b32e319c3f25170
sergeyinstigatemobile <sergey@instigatemobile.com>
2023-04-06 18:42:39 +0400

    Update social media icons

--- viva-child/js/custom.js
+++ viva-child/js/custom.js
@@ -2,6 +2,7 @@
   $(document).ready(function () {
     ("use strict");
 
+<<<<<<< Updated upstream
     /* Add About Us section icons */
     $(".fa-chalkboard-teacher")
       .removeClass("fas fa-chalkboard-teacher")
@@ -15,5 +16,28 @@
     $(".fa-handshake")
       .removeClass("far fa-handshake")
       .addClass("fa-light fa-hands-holding-diamond");
+=======
+    // Update social media icons
+    var $linkedinIcon = $(".fa-linkedin");
+    var $linkedinImg = $(
+      '<img src="/wp-content/uploads/2023/03/icons8-linkedin-2-1.svg" class="header-logo" alt="Linkedin" title="Linkedin">'
+    );
+    $linkedinImg.insertBefore($linkedinIcon);
+    $linkedinIcon.remove();
+
+    var $xingIcon = $(".fa-xing");
+    var $linkedinImg = $(
+      '<img src="/wp-content/uploads/2023/03/xing-icon.svg" class="header-logo" alt="Xing" title="Xing">'
+    );
+    $linkedinImg.insertBefore($xingIcon);
+    $xingIcon.remove();
+
+    var $youtubeIcon = $(".fa-youtube");
+    var $linkedinImg = $(
+      '<img src="/wp-content/uploads/2023/03/youtube-icon.svg" class="youtube-logo" alt="Youtube" title="Youtube">'
+    );
+    $linkedinImg.insertBefore($youtubeIcon);
+    $youtubeIcon.remove();
+>>>>>>> Stashed changes
   });
 })(jQuery);
