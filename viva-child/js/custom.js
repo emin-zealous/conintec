965911344b96108165dcc83b4cbf4fe65f8d7b67
sergeyinstigatemobile <sergey@instigatemobile.com>
2023-04-12 14:17:02 +0400

    Code optimization

--- viva-child/js/custom.js
+++ viva-child/js/custom.js
@@ -2,18 +2,17 @@
   $(document).ready(function () {
     ("use strict");
 
-    // Add images for workflow section
-    $(".first-image > div > a > .timeline-item-date").append(
-      '<img width="25%" height="auto" style="max-width: 80px; margin: -10px" src="/wp-content/uploads/2023/03/foto_telescope_284-1-1.jpg">'
-    );
-    $(".second-image > div > a > .timeline-item-date").append(
-      '<img width="25%" height="auto" style="max-width: 80px; margin: -10px" src="/wp-content/uploads/2023/03/foto_puzzle_284-1-1.jpg">'
-    );
-    $(".thirdth-image > div > a > .timeline-item-date").append(
-      '<img width="25%" height="auto" style="max-width: 80px; margin: -10px" src="/wp-content/uploads/2023/03/computer_hand_288x284.png">'
-    );
-    $(".fourth-image > div > a > .timeline-item-date").append(
-      '<img width="25%" height="auto" style="max-width: 80px; margin: -10px" src="/wp-content/uploads/2023/03/Hands-1-1.jpg">'
-    );
+    const images = {
+      "first-image": "/wp-content/uploads/2023/04/foto_telescope_284-1-1.webp",
+      "second-image": "/wp-content/uploads/2023/04/foto_puzzle_284-1-1.webp",
+      "thirdth-image": "/wp-content/uploads/2023/04/computer_hand_288x284.webp",
+      "fourth-image": "/wp-content/uploads/2023/04/Hands-1-1.webp",
+    };
+
+    for (const [className, src] of Object.entries(images)) {
+      $(`.${className} > div > a > .timeline-item-date`).append(
+        `<img width="25%" height="auto" style="max-width: 80px; margin: -10px" src="${src}">`
+      );
+    }
   });
 })(jQuery);
