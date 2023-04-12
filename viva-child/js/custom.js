3e4ccdb597e7b873d2be550e68e475feb7b31c47
sergeyinstigatemobile <sergey@instigatemobile.com>
2023-04-12 14:59:29 +0400

    Fixed merging issue and added icon sizes

--- viva-child/js/custom.js
+++ viva-child/js/custom.js
@@ -2,7 +2,6 @@
   $(document).ready(function () {
     ("use strict");
 
-<<<<<<< Updated upstream
     /* Add About Us section icons */
     $(".fa-chalkboard-teacher")
       .removeClass("fas fa-chalkboard-teacher")
@@ -16,28 +15,27 @@
     $(".fa-handshake")
       .removeClass("far fa-handshake")
       .addClass("fa-light fa-hands-holding-diamond");
-=======
+
     // Update social media icons
     var $linkedinIcon = $(".fa-linkedin");
     var $linkedinImg = $(
-      '<img src="/wp-content/uploads/2023/03/icons8-linkedin-2-1.svg" class="header-logo" alt="Linkedin" title="Linkedin">'
+      '<img width="34px" height="34px" src="/wp-content/uploads/2023/03/icons8-linkedin-2-1.svg" class="header-logo" alt="Linkedin" title="Linkedin">'
     );
     $linkedinImg.insertBefore($linkedinIcon);
     $linkedinIcon.remove();
 
     var $xingIcon = $(".fa-xing");
     var $linkedinImg = $(
-      '<img src="/wp-content/uploads/2023/03/xing-icon.svg" class="header-logo" alt="Xing" title="Xing">'
+      '<img width="34px" height="34px" src="/wp-content/uploads/2023/03/xing-icon.svg" class="header-logo" alt="Xing" title="Xing">'
     );
     $linkedinImg.insertBefore($xingIcon);
     $xingIcon.remove();
 
     var $youtubeIcon = $(".fa-youtube");
     var $linkedinImg = $(
-      '<img src="/wp-content/uploads/2023/03/youtube-icon.svg" class="youtube-logo" alt="Youtube" title="Youtube">'
+      '<img width="34px" height="34px" src="/wp-content/uploads/2023/03/youtube-icon.svg" class="youtube-logo" alt="Youtube" title="Youtube">'
     );
     $linkedinImg.insertBefore($youtubeIcon);
     $youtubeIcon.remove();
->>>>>>> Stashed changes
   });
 })(jQuery);
