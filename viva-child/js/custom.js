4aee261ff11f305706af6b9052a6a8c3fb48c9e3
sergeyinstigatemobile <sergey@instigatemobile.com>
2023-04-21 11:28:42 +0400

    Improve code readability and maintainability with language object

--- viva-child/js/custom.js
+++ viva-child/js/custom.js
@@ -2,13 +2,25 @@
   $(document).ready(function () {
     ("use strict");
 
+    // Define the language URLs
+    const langUrls = {
+      "en-US": {
+        searchUrl: "/portfolio-news/",
+        replaceUrl: "/news/#",
+      },
+      "de-DE": {
+        searchUrl: "/portfolio-news/",
+        replaceUrl: "/news-center/#",
+      },
+    };
+
     // Replace news section URLs
     $("a[href*='portfolio-news']").attr("href", function (i, val) {
-      if ($("html").attr("lang") === "en-US") {
-        return val.replace("/portfolio-news/", "/news/#").replace(/\/$/, "");
-      } else if ($("html").attr("lang") === "de-DE") {
+      const lang = $("html").attr("lang");
+      const langUrl = langUrls[lang];
+      if (langUrl) {
         return val
-          .replace("/portfolio-news/", "/news-center/#")
+          .replace(langUrl.searchUrl, langUrl.replaceUrl)
           .replace(/\/$/, "");
       }
     });
